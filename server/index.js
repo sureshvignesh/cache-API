d40508f7d2818c6ddb66870f35be8888bd969303
Vignesh Suresh <pingviky@gmail.com>
2018-10-06 13:59:28 +0530

    Set up index.js

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const express = require('express')
+// const routine = require('./api-v1/routes/routine')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const port = process.env.PORT ? process.env.PORT : 8181
+const dist = path.join(__dirname, '../dist')
+
+app.use(express.static(dist))
+app.use(bodyParser.json()) // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true
+}))
+
+// app.use('/api/v1/routine', routine)
+
+app.get('*', (req, res) => {
+  res.send('Hello')
+})
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log(error) // eslint-disable-line no-console
+  }
+  console.info('Express is listening on port %s.', port) // eslint-disable-line no-console
+})
